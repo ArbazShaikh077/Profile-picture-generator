fa2204054da63c9f9d700d831f734d2ae6993ee7
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-01-02 22:23:58 +0530

    Enable material 3 ui

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
