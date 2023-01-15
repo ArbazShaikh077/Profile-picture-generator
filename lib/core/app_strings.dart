c0cef0aeefeb4957c720625206e6209272e265b7
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-01-15 21:43:11 +0530

    Added the main screen string

--- lib/core/app_strings.dart
+++ lib/core/app_strings.dart
@@ -2,4 +2,5 @@ class AppStrings {
   static const String appName = 'ProfilePictureGenerator';
   static const String login = "Login In";
   static const String signup = "Sign Up";
+  static const speakToConvert = "Speak to get you answer";
 }
