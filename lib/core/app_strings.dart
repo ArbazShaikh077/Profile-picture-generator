9bac885cb2824b25f779787a4bf0d4a330398268
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-01-16 21:29:18 +0530

    Modifing strings file

--- lib/core/app_strings.dart
+++ lib/core/app_strings.dart
@@ -2,5 +2,6 @@ class AppStrings {
   static const String appName = 'ProfilePictureGenerator';
   static const String login = "Login In";
   static const String signup = "Sign Up";
-  static const speakToConvert = "Speak to get you answer";
+  static const String speakToConvert = "Speak to get you answer";
+  static const String pleaseWait = "Please wait while processing";
 }
